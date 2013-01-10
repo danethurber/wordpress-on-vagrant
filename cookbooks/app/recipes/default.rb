cd97d881bdf447600be3151de8247257c63e2b53
Dane Thurber <dane.thurber@gmail.com>
2013-01-09 20:08:48 -0700

    install and provision wordpress

--- cookbooks/app/recipes/default.rb
+++ cookbooks/app/recipes/default.rb
@@ -1,2 +1,13 @@
 require_recipe "apt"
 require_recipe "build-essential"
+
+require_recipe "mysql"
+require_recipe "php"
+require_recipe "apache2"
+require_recipe "openssl"
+require_recipe "wordpress"
+
+execute "replace themes folder" do
+  command "sudo rm -rf /var/www/wordpress/wp-content/themes && sudo ln -s /vagrant/themes /var/www/wordpress/wp-content/themes"
+  action :run
+end
